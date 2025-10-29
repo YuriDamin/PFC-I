30774f8d86de1e125da079e7df5e14d2af6ea963
YuriDamin <yuribonfant@gmail.comgit config --global user.name YuriDamingit config --get-all user.namegit config --global user.email yuribonfant@gmail.com>
2025-10-29 00:44:36 -0300

    Hide header on login page and improve routing logic

--- meu-museu/src/App.jsx
+++ meu-museu/src/App.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import ConsolePage from './pages/ConsolePage';
 import GamePage from './pages/GamePage';
 import LoginPage from './pages/LoginPage';
 import { AuthProvider } from './context/AuthContext';
-import { XPProvider } from './context/XPContext'; // 🧩 importação adicionada
+import { XPProvider } from './context/XPContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
 
 function App() {
+  const location = useLocation();
+
+  // Esconde o Header quando estiver na tela de login
+  const hideHeader = location.pathname === '/login';
+
   return (
     <div className="App">
       <AuthProvider>
         <XPProvider>
-          <Header />
+          {!hideHeader && <Header />}
           <main>
             <Routes>
               <Route path="/login" element={<LoginPage />} />
